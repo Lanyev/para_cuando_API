4e5a1a8ae4a9fd8cfc03d87e1dfcbde477cb9eb9
Julio Sanquez <julio930911@gmail.com>
2023-03-19 21:37:28 -0600

    feat: Added services, controllers & routes of states, cities, tags & publications_types, 2

--- routes/models.router.js
+++ routes/models.router.js
@@ -1,23 +1,18 @@
 const express = require('express');
+const routesUsers = require('./users.routes');
 
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
 const routesAuth = require('./auth.routes');
-const routesCities = require('./cities.routes');
 const routesCountries = require('./countries.routes');
-const routesStates = require('./states.routes');
-const routesUsers = require('./users.routes');
 
 function routerModels(app) {
   const router = express.Router();
 
   app.use('/api/v1', router);
   router.use('/auth', routesAuth);
-  router.use('/cities', routesCities);
   router.use('/countries', routesCountries);
-  router.use('/states', routesStates);
   router.use('/users', routesUsers);
-  
 }
 
 module.exports = routerModels;
